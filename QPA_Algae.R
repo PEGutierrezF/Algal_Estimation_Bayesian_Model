7a970cc78b3f0bc827078d40496533a47e8d4b26
Pablo E. Gutierrez-Fonseca <pabloe.gutierrezfonseca@gmail.com>
2020-09-02 14:06:54 -0600

    Model QPA

--- QPA_Algae.R
+++ QPA_Algae.R
@@ -225,10 +225,12 @@ parameters{
     ordered [src_no] src_C_mean;
     ordered [src_no] src_N_mean;
     simplex [src_no] Theta;
-    vector <lower=0> [src_no] e_C;  // residual error representing additional unquantified
-                                    // variation between individual; normally distributed with
-                                    // mean = 0 and standard deviation= ??. (Jackson et al. 2009)
-    vector <lower=0> [src_no] e_N;  // Residual error Jackson et al. 2009
+    
+    // residual error (e_) representing additional unquantified
+    // variation between individual; normally distributed with
+    // mean = 0 and standard deviation= ??. (Jackson et al. 2009)
+    vector <lower=0> [src_no] e_C;  
+    vector <lower=0> [src_no] e_N;
     vector [src_no] sigma_C;
     vector [src_no] sigma_N;
     }
@@ -239,36 +241,36 @@ transformed parameters{
     vector [src_no] mu_N;
     vector [src_no] Theta2;
     vector [src_no] Theta_mean;
-    vector [src_no] ilr.global;
+    vector [src_no] ilr_global;
     
     // 13C Stable Isotopes
     for (k in 1:src_no){
-    mu_C[k] <- src_C_mean[k] + DeltaC * L;
+    mu_C[k] = src_C_mean[k] + DeltaC * L;
     }
     
     // 15N Stable Isotopes
     for (k in 1:src_no){
-    mu_N[k] <- src_N_mean[k] + DeltaN * L;
+    mu_N[k] = src_N_mean[k] + DeltaN * L;
     }
     
     // to integrate proportion on sigma estimation
     for(k in 1:src_no){
-    Theta2[k] <- Theta[k]^2;
+    Theta2[k] = Theta[k]^2;
     }
     
     // draw p.global (global proportion means) from an uninformative Dirichlet,
-    // then ilr.global is the ILR-transform of p.global. Egozcue 2003 (pages 296)
-    // Pavel -> Theta.  
+    // then ilr_global is the ILR-transform of p.global. Egozcue 2003 (pages 296)
+    // Pavel: gmean = Theta, k = src.  
     for(k in 1:(src_no - 1)){
-    Theta_mean <- rows_dot_self(Theta[k]) ^ (1/k);
-    ilr.global[k] <- sqrt(k/(k+1)) * log(Theta_mean[k]/Theta[k+1]); // page 296, Egozcue 2003
+    Theta_mean = rows_dot_self(Theta[k])^(1/k);
+    ilr_global[k] = sqrt(k/(k+1)) * log(Theta_mean[k]/Theta[k+1]); // page 296, Egozcue 2003
     }
     
     // DON'T generate individual deviates from the global/region/pack mean (but keep same model structure)
     for(i in 1:N){
-    for(src in 1:(n.sources - 1)){
-        ilr.ind[i,src] <- 0;
-        ilr.total[i,src] <- ilr.global[src] + ilr.ind[i,src]; // add all effects together for each individual (in ilr-space)
+    for(src in 1:(n.sources-1)) {
+        ilr_ind[i,src] = 0;
+        ilr_total[i,src] = ilr_global[src] + ilr_ind[i,src]; // add all effects together for each individual (in ilr-space)
         }
     }
 }
